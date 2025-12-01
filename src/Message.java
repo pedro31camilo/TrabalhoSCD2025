2f708ca9ff9f8413320602c7710d67728a67f09c
km1l0 <pedrocamilo536@gmail.com>
2025-12-01 16:58:12 -0300

    Message class

---
+++ src/Message.java
@@ -0,0 +1,49 @@
+public class Message {
+    public static final int TAMANHO_MENSAGEM = 10;
+    public static final String SEPARADOR = "|";
+
+    public final int type;
+    public final int processId;
+
+    public Message(int type, int processId){
+        this.type = type;
+        this.processId = processId;
+    }
+
+    public int getType() { return type; }
+    public int getProcessId() { return processId; }
+
+    public String transformaMessage(){ //Transforma o Objeto Message em String
+        String message = type + SEPARADOR + processId + SEPARADOR;
+
+        int zeros = TAMANHO_MENSAGEM - message.length();
+
+        String filling = String.format("%" + zeros + "s", "").replace(" ", "0");
+        return message + filling;
+    }
+
+    public Message transformaString(String mensagem){
+        String[] texts = mensagem.split("\\" + SEPARADOR);
+        if(texts.length >= 2) {
+            try{
+                int type = Integer.parseInt(texts[0]);
+                int processId = Integer.parseInt(texts[1]);
+                return new Message(type, processId);
+            } catch (NumberFormatException e){
+                System.err.println("Parsing error: " + mensagem);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        String typeStr;
+        if(type == 1){
+            typeStr = "REQUEST";
+        } else if(type == 2){
+            typeStr = "GRANT";
+        } else typeStr = "RELEASE";
+        return String.format("%s (ID: %d)", typeStr, processId);
+    }
+}
