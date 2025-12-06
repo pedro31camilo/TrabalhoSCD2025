3b38bdd3dfc4e58fd23d13097fe6ae5d1d8aa738
JoelRTavares <joelrtavares2017@gmail.com>
2025-12-06 11:32:31 -0300

    Limpando FileHelper

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -2,23 +2,9 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import java.time.LocalDateTime;
-
 public class FileHelper
 {
-    /*
-    Exemplos de uso
-    public static void main(String[] args) {
-        LocalDateTime agora = LocalDateTime.now();
-        appendToFile(agora + " Operacao de Request", "dados.txt");
-
-        LocalDateTime depois = LocalDateTime.now();
-        appendToFile(depois + " Uma operação de Grant.", "dados.txt");
-    }
-    */
-
     public static boolean appendToFile(String conteudo, String nomeArquivo) {
-        // Validação básica dos parâmetros
         if (conteudo == null || nomeArquivo == null || nomeArquivo.trim().isEmpty()) {
             System.err.println("Parâmetros inválidos");
             return false;
